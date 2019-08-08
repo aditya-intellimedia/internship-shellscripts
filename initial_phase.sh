6c400d1cdd962dacb720de9cb65aa10ca8565b9c
Vallabhjr <53888607+Vallabhjr@users.noreply.github.com>
2019-08-08 15:18:09 +0530

    Create initial_phase.sh

---
+++ initial_phase.sh
@@ -0,0 +1,9 @@
+#! /been/sh
+echo"Enter the directory name: "
+read $directory_name
+if[ -d $directory_name]
+then
+echo"Here will be the link to the further code"
+else
+echo"This Directory doesn't exist"
+fi
