a87ab21d34b643ce3a9a2eae8cc9c827b8a2b610
Vallabhjr <53888607+Vallabhjr@users.noreply.github.com>
2019-08-09 18:17:46 +0530

    Update day2.sh

--- day2.sh
+++ day2.sh
@@ -3,7 +3,7 @@ echo"Enter the directory name: "
 read $directory_name
 if [ -d $directory_name ]
 then
-mv /$directory_name/*.txt ~/temp
+mv $directory_name/*.txt ~/temp
 else
 echo "This Directory doesn't exist"
 fi
