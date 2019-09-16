adf6625bafc9de6f8681910294cb7190a0a08fc4
Vallabhjr <53888607+Vallabhjr@users.noreply.github.com>
2019-09-16 14:51:00 +0530

    Update final code.sh

--- auto_vm _creation/final code.sh
+++ auto_vm _creation/final code.sh
@@ -1,9 +1,36 @@
 #! /bin/bash
 
 #doctl auth init
+echo "Droplet Name:"
+read droplet_name
 
+echo echo "Image Name:"
+echo " 1.Ubuntu    2.CentOS    3.Debian "
+echo "Press coresponding no."
+read no
+if [[ $no == "1" ]]
+then imag="ubuntu-18-04-x64"
+elif [[ $no == "2" ]]
+then imag="centos-6-5-x64"
+elif [[ $no == "3" ]]
+then imag="debian-9-x64"
+else
+echo "Invalid entry"
+fi
+echo "Region :"
+echo " 1.Newyork1   2.Newyork2   3.Newyork3 "
+echo "Press coresponding no."
+read nom
+if [[ $nom == "1" ]]
+then reg="nyc1"
+elif [[ $nom == "2" ]]
+then reg="nyc2"
+elif [[ $nom == "3" ]]
+then reg="nyc3"
+else echo "Invalid entry"
+fi
 echo "Droplet is being created"
-doctl compute droplet create het --region nyc1 --image ubuntu-18-04-x64 --size 1gb --ssh-keys 25336801 --wait
+doctl compute droplet create $droplet_name --region $reg --image $imag --size 1gb --ssh-keys 25336801 --wait
 
 echo "Droplet is created"
 doctl compute ssh het --ssh-command "sudo apt update"
