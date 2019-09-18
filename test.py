378cc87f6409a6b0913c90d8d3ce16d705f40210
Vallabhjr <53888607+Vallabhjr@users.noreply.github.com>
2019-09-18 15:37:37 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+print("Hello world")
+a=6
+print(str(a) + "hello")
+
+if a>=5:
+    print("okayy")
+else:
+    print("not okayy")
+
+str='Hello Pep'
+print(str.isalpha())
+print(str.isdigit())
+print(str.upper())
+print(str.lower())
+print(str[:])
+print(str[:3])
+print(str[5:9])
+
+if a >= 0 :
+    print("License and registration please")
+    if a >= 100:
+            print( 'You have the right to remain silent.')
+     elif a >= 90:
+            print ("I'm going to have to write you a ticket.")
+     else:
+            print ("Let's try to keep it under 80 ok?")
+      
+      
